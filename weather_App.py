abc481f8f22892735f187ed8e4b9d708c79ebfa6
Alexian <alexian26@Alexian.myguest.virtualbox.org>
2024-02-05 02:04:39 -0600

    add weather_app.py

---
+++ weather_App.py
@@ -0,0 +1,69 @@
+import os
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import requests
+from twilio.rest import Client
+from twilio_config import *
+import time
+
+import json
+import pandas as pd
+
+from bs4  import BeautifulSoup
+from tqdm import tqdm
+from datetime import datetime
+
+query = 'Guadalajara'
+api_key = API_KEY_WAPI
+
+url_clima = 'http://api.weatherapi.com/v1/forecast.json?key='+api_key+'&q='+query+'&days=1&aqi=no&alerts=no'
+
+response = requests.get(url_clima).json()
+
+
+def get_forecast(response,i):
+    
+    fecha = response['forecast']['forecastday'][0]['hour'][i]['time'].split()[0]
+    hora = int(response['forecast']['forecastday'][0]['hour'][i]['time'].split()[1].split(':')[0])
+    condicion = response['forecast']['forecastday'][0]['hour'][i]['condition']['text']
+    tempe = float(response['forecast']['forecastday'][0]['hour'][i]['temp_c'])
+    rain = response['forecast']['forecastday'][0]['hour'][i]['will_it_rain']
+    prob_rain = response['forecast']['forecastday'][0]['hour'][i]['chance_of_rain']
+    
+    return fecha,hora,condicion,tempe,rain,prob_rain
+
+datos = []
+
+for i in tqdm(range(len(response['forecast']['forecastday'][0]['hour'])),colour = 'green'):
+    
+    datos.append(get_forecast(response,i))
+    
+col = ['Fecha','Hora','Condicion','Temperatura','Lluvia','prob_lluvia']
+df = pd.DataFrame(datos,columns=col)
+df = df.sort_values(by = 'Hora',ascending = True)
+
+df_rain =  df[(df['Lluvia']==1) & (df['Hora']>6) & (df['Hora']< 22)]
+df_rain = df_rain[['Hora','Condicion']]
+df_rain.set_index('Hora', inplace = True)
+
+if df_rain.empty:
+    message = '\nHola! \n\n\n El pronostico de lluvia hoy '+ df['Fecha'][0] +' en ' + query +' es : \n\n\n ' + 'No hay lluvia'
+else:
+    message = '\nHola! \n\n\n El pronostico de lluvia hoy '+ df['Fecha'][0] +' en ' + query +' es : \n\n\n ' + str(df_rain)
+    
+print(message)
+
+time.sleep(2)
+account_sid = TWILIO_ACCOUNT_SID 
+auth_token = TWILIO_AUTH_TOKEN
+
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body=message,
+                     from_=PHONE_NUMBER,
+                     to='+52 33 3060 1213'
+                 )
+
+print('Mensaje Enviado ' + message.sid)
